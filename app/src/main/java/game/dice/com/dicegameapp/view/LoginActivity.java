5f66f30c61ea399eed8f9585162aba00317b9a98
alberto <albertofgaldo@gmail.com>
2018-09-04 12:40:17 +0200

    Martes 4/09

--- app/src/main/java/game/dice/com/dicegameapp/view/LoginActivity.java
+++ app/src/main/java/game/dice/com/dicegameapp/view/LoginActivity.java
@@ -2,6 +2,7 @@ package game.dice.com.dicegameapp.view;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,41 +22,28 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         usuario=(TextView)findViewById(R.id.userText);
-
-        //EventoTeclado teclado=new EventoTeclado();
-       // usuario.setOnEditorActionListener(teclado);
-
     }
 
     public void checkLogin(View vista)throws Exception{
+        final Intent i = new Intent(this,MenuActivity.class);
 
         AlertDialog.Builder builder=new AlertDialog.Builder(LoginActivity.this);
         builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //mostramos la activity menu al pulsar OK
+                startActivity(i);
+            }
+        });
 
         if (gameController.existPlayer(usuario.getText().toString())){
-            builder.setTitle("Usuario existente");
-            //devolver el usuario del repositorio
-            builder.setMessage("tu usuario ya existe, vas a acceder a tu menú");
+            builder.setTitle("Bienvenido " +  usuario.getText().toString());
         }else{
             gameController.createPlayer(usuario.getText().toString());
-            builder.setTitle("Usuario creado");
-            builder.setMessage("tu usuario ha sido creado");
+            builder.setTitle("Usuario " +  usuario.getText().toString() +" creado");
         }
+
         builder.show();
-        Intent i = new Intent(this,MenuActivity.class);
-        startActivity(i);
+
     }
-    //Cuando pulsamos en el teclado intro = GO!
-    /*class EventoTeclado implements TextView.OnEditorActionListener{
-
-        @Override
-        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent){
-
-            if(i== EditorInfo.IME_ACTION_DONE){
-                checkLogin(null);
-            }
-            return false;
-        }
-    }*/
-
 }
